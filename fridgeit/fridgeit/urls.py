421aede0d9e2935db95b366182de2b9690b0b7a0
April <aluo@salesforce.com>
2014-01-25 10:30:08 -0800

    landing page bwaaahhhh

--- fridgeit/fridgeit/urls.py
+++ fridgeit/fridgeit/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'fridgeit.views.home', name='home'),
+     url(r'^$', 'fridgeit.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- fridgeit/fridgeit/views.py
+++ fridgeit/fridgeit/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
+	return render(request, 'landing.html')
+
+def index(request):
 	return render(request, 'index.html')
