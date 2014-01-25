4332ff9cd80fa8881290a352a1b94b777fc00d92
Chris McLaughlin <chrismclaughlin55@gmail.com>
2014-01-25 01:52:33 -0800

    working api call

---
+++ tester.py
@@ -0,0 +1,8 @@
+from pinterest.models.model import Pinterest, User
+
+CLIENT_SECRET = '56de81f97190ff657e0f828d54185cf527316fb3'
+CLIENT_ID = '1435704'
+Pinterest.configure_client(CLIENT_ID, CLIENT_SECRET)
+me = User('tcpanda')
+print me.id
+
