3132ee63d27ea367ca6d65d9c1a3de74ec10607b
Chris McLaughlin <chrismclaughlin55@gmail.com>
2014-01-25 13:43:02 -0800

    views

--- fridgeit/fridgeit/views.py
+++ fridgeit/fridgeit/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import logout, login
+from django.contrib.auth import logout, login, authenticate
 from django.http import HttpResponseRedirect
 from fridgeit.models import User
 from pinterest.models.model import Pinterest
@@ -9,16 +9,28 @@ CLIENT_ID = "1435790"
 CLIENT_SECRET = "8c8eab09fe710377c9e879872855109c9f349195"
 Pinterest.configure_client(CLIENT_ID, CLIENT_SECRET)
 
+from django.core.urlresolvers import reverse
+from fridgeit.models import User
+
 def home(request):
 	return render(request, 'landing.html')
+
+def get_user(email):
+    try:
+        return User.objects.get(email=email.lower())
+    except User.DoesNotExist:
+        return None
+
 def login(request):
 	if request.method =="POST":
 		email = request.POST.get('email')
 		password = request.POST.get('password')
-		user = authenticate(email=email, password=password)
+		username = get_user(email)		
+		user = authenticate(username = username, password=password)
 		if user is not None:
 			if user.is_active:
 				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
 	return HttpResponseRedirect('/')
 
 def logout_page(request):
