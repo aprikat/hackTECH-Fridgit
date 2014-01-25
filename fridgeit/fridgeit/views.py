7397c58377000234f0628b1cd349398281d11449
Chris McLaughlin <chrismclaughlin55@gmail.com>
2014-01-25 10:34:57 -0800

    more merge

--- fridgeit/fridgeit/views.py
+++ fridgeit/fridgeit/views.py
@@ -3,18 +3,11 @@ from django.contrib.auth import logout, login
 from django.http import HttpResponseRedirect
 
 def home(request):
-<<<<<<< HEAD
-	return render(request, 'index.html')
+	return render(request, 'landing.html')
 
 def logout_page(request):
 	logout(request)
 	return HttpResponseRedirect('/')
-
-
-
-=======
-	return render(request, 'landing.html')
-
+	
 def index(request):
 	return render(request, 'index.html')
->>>>>>> 421aede0d9e2935db95b366182de2b9690b0b7a0
