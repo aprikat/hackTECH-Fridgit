916790f0726619b52d064d17a19c7b8b5be530f3
Chris McLaughlin <chrismclaughlin55@gmail.com>
2014-01-25 07:36:55 -0800

    added django stuff

--- tester.py
+++ tester.py
@@ -4,5 +4,10 @@ CLIENT_SECRET = '56de81f97190ff657e0f828d54185cf527316fb3'
 CLIENT_ID = '1435704'
 Pinterest.configure_client(CLIENT_ID, CLIENT_SECRET)
 me = User('tcpanda')
-print me.id
+pins = me.pins()
+pin = pins[0]
+pin.
+print pin.comments()
+boards = pin.related_boards()
+followers = boards[0].followers()
 
